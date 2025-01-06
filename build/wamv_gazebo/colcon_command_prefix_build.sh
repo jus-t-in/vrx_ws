b70d21db608cadbce760e6baafa572d92f8c927c
justin <hzhou0799@gmail.com>
2025-01-06 15:29:50 +0800

    first commit

---
+++ build/wamv_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/zjh/vrx_ws/install/share/vrx_gazebo/package.sh"
+. "/home/zjh/vrx_ws/install/share/wamv_description/package.sh"
